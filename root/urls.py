61f429cae978674a36be0b54b799465302d70eb4
Joe Philip <joe.philip@hotmail.co.in>
2023-04-07 16:09:17 +0530

    added static url conf

--- root/urls.py
+++ root/urls.py
@@ -22,4 +22,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls'))
-]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
