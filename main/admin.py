a26346ac53a284ef7912faaa49efd1a195a99afa
Joe Philip <joe.philip@hotmail.co.in>
2023-10-17 10:12:53 +0530

    modified admin pannel for all

--- main/admin.py
+++ main/admin.py
@@ -30,13 +30,35 @@ class ProjectLinksAdmin(admin.ModelAdmin):
     list_filter = ('project', 'project__profile')
 
 
+class ProjectsAdmin(admin.ModelAdmin):
+    list_filter = ('profile',)
+
+
+class WorkExperienceRolesAndResponsibilitiesAdmin(admin.ModelAdmin):
+    list_filter = ('experience',)
+
+
+class WorkExperienceAdditionalDataAdmin(admin.ModelAdmin):
+    list_filter = ('experience',)
+
+
+class WorkExperienceAdmin(admin.ModelAdmin):
+    list_filter = ('profile',)
+
+
 admin.site.register(ContactUs, ContactUsAdmin)
 admin.site.register(Profile)
 admin.site.register(Skills)
 admin.site.register(SocialMedia)
 admin.site.register(WorkExperience)
-admin.site.register(WorkExperienceAdditionalData)
-admin.site.register(WorkExperienceRolesAndResponsibilities)
-admin.site.register(Projects)
+admin.site.register(
+    WorkExperienceAdditionalData,
+    WorkExperienceAdditionalDataAdmin
+)
+admin.site.register(
+    WorkExperienceRolesAndResponsibilities,
+    WorkExperienceRolesAndResponsibilitiesAdmin
+)
+admin.site.register(Projects, ProjectsAdmin)
 admin.site.register(ProjectLinks, ProjectLinksAdmin)
 admin.site.register(ContactInfo, ContactInfoAdmin)
