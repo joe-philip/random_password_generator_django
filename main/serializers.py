2567513282811a0d54326c5880282f7612979a2b
Joe Philip <joe.philip@hotmail.co.in>
2023-10-20 11:53:23 +0530

    added ordering for work experience

--- main/serializers.py
+++ main/serializers.py
@@ -127,7 +127,9 @@ class ProfileSerializer(serializers.ModelSerializer):
     contact_info = serializers.SerializerMethodField()
 
     def get_work_experience(self, instance: Profile) -> OrderedDict or list:
-        queryset = WorkExperience.objects.filter(profile=instance)
+        queryset = WorkExperience.objects.filter(
+            profile=instance
+        ).order_by('created_at')
         return WorkExperienceSerializer(queryset, context=self.context, many=True).data
 
     def get_projects(self, instance: Profile) -> OrderedDict or list:
